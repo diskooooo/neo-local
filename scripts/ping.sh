f58884d034af9f788d5186ce9223dde27a20f208
CryptoTuga <nuno@justoweb.com>
2018-12-01 12:37:24 +0000

    testing execution of autoheal container/service

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -7,4 +7,10 @@ while [ "$(docker inspect --format '{{json .State.Health.Status }}' neo-scan-api
     sleep 3
   done
 
+while [ "$(docker inspect --format '{{json .State.Health.Status }}' neo-local_autoheal)" != "\"healthy\"" ]
+  do
+    printf '.'
+    sleep 3
+  done
+
 printf "\n"
